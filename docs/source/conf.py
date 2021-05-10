5ab8b1ad1132da35a02fba3ab5a8f6e51ae7e030
lorr <lorr@noc.ac.uk>
2021-05-10 16:29:19 +0100

    Update Latex output in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,3 +57,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Options for LaTeX output -------------------------------------------------
+
+# Remove blank pages in PDF output
+latex_elements = {
+  'extraclassoptions': 'openany,oneside'
+}
